b0e4ee447f3c33fa0a80755aaf11fc2f6d4e053f
arunsachan <66840949+arunsachan@users.noreply.github.com>
2023-02-18 16:36:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // your code here
+
+let fom= document.querySelector('form')
+let nInput=document.getElementById('name');
+let yInput=document.getElementById('year');
+let btn=document.getElementById('button');
+let head= document.getElementById('url');
+
+function handleSubmit(e){
+    e.preventDefault();
+    h3Value= 'https://localhost:8080/';
+    let name = nInput.value;
+    let date = yInput.value;
+    
+    if(name && date){
+        h3Value += '?name=' + name + '&year=' + date;
+    }
+     else if(name && !date){
+       h3Value +=  '?name=' + name
+     }
+     else if(!name && date){
+        h3Value += '&year=' + date ;
+     }
+  head.innerText=h3Value;
+}
+
+fom.addEventListener('submit' , handleSubmit);
